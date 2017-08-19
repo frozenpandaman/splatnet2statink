7cc982c4dce37674f9b6fab81068dee7a856a588
eli fessler <eliwf8@gmail.com>
2017-08-19 02:26:16 -0500

    fixed auth error handling for cookie acquisition

--- iksm.py
+++ iksm.py
@@ -27,15 +27,20 @@ def get_cookie(session_token):
 	id_response = json.loads(r.text)
 
 	# get user info
-	app_head = {
-		'User-Agent': 'OnlineLounge/1.0.4 NASDKAPI Android',
-		'Accept-Language': 'en-US',
-		'Accept': 'application/json',
-		'Authorization': 'Bearer ' + id_response["access_token"],
-		'Host': 'api.accounts.nintendo.com',
-		'Connection': 'Keep-Alive',
-		'Accept-Encoding': 'gzip'
-	}
+	try:
+		app_head = {
+			'User-Agent': 'OnlineLounge/1.0.4 NASDKAPI Android',
+			'Accept-Language': 'en-US',
+			'Accept': 'application/json',
+			'Authorization': 'Bearer ' + id_response["access_token"],
+			'Host': 'api.accounts.nintendo.com',
+			'Connection': 'Keep-Alive',
+			'Accept-Encoding': 'gzip'
+		}
+	except:
+		print "Not a valid authorization request. (Did you enter your session_token correctly?)"
+		print "Error from Nintendo:\n" + json.dumps(id_response, indent=2)
+		exit(1)
 	url = "https://api.accounts.nintendo.com/2.0.0/users/me"
 
 	r = requests.get(url, headers=app_head)
@@ -57,12 +62,17 @@ def get_cookie(session_token):
 	}
 
 	body = {}
-	parameter = {
-		'naIdToken': id_response["id_token"],
-		'naCountry': user_info["country"],
-		'naBirthday': user_info["birthday"],
-		'language': user_info["language"]
-	}
+	try:
+		parameter = {
+			'naIdToken': id_response["id_token"],
+			'naCountry': user_info["country"],
+			'naBirthday': user_info["birthday"],
+			'language': user_info["language"]
+		}
+	except:
+		print "Error(s) from Nintendo:\n" + json.dumps(id_response, indent=2)
+		print json.dumps(user_info, indent=2)
+		exit(1)
 	body["parameter"] = parameter
 
 	url = "https://api-lp1.znc.srv.nintendo.net/v1/Account/Login"
@@ -71,19 +81,23 @@ def get_cookie(session_token):
 	splatoon_token = json.loads(r.text)
 
 	# get splatoon access token
-	app_head = {
-		'Host': 'api-lp1.znc.srv.nintendo.net',
-		'Accept-Language': 'en-us',
-		'User-Agent': 'com.nintendo.znca/1.0.4 (Android/7.1.2)',
-		'Accept': 'application/json',
-		'X-ProductVersion': '1.0.4',
-		'Content-Type': 'application/json; charset=utf-8',
-		'Connection': 'keep-alive',
-		'Authorization': 'Bearer ' + splatoon_token["result"]["webApiServerCredential"]["accessToken"],
-		'Content-Length': '37',
-		'X-Platform': 'Android',
-		'Accept-Encoding': 'gzip, deflate'
-	}
+	try:
+		app_head = {
+			'Host': 'api-lp1.znc.srv.nintendo.net',
+			'Accept-Language': 'en-us',
+			'User-Agent': 'com.nintendo.znca/1.0.4 (Android/7.1.2)',
+			'Accept': 'application/json',
+			'X-ProductVersion': '1.0.4',
+			'Content-Type': 'application/json; charset=utf-8',
+			'Connection': 'keep-alive',
+			'Authorization': 'Bearer ' + splatoon_token["result"]["webApiServerCredential"]["accessToken"],
+			'Content-Length': '37',
+			'X-Platform': 'Android',
+			'Accept-Encoding': 'gzip, deflate'
+		}
+	except:
+		print "Error from Nintendo:\n" + json.dumps(splatoon_token, indent=2)
+		exit(1)
 
 	body = {}
 	parameter = {
@@ -97,18 +111,22 @@ def get_cookie(session_token):
 	splatoon_access_token = json.loads(r.text)
 
 	# get cookie
-	app_head = {
-		'Host': 'app.splatoon2.nintendo.net',
-		'X-IsAppAnalyticsOptedIn': 'true',
-		'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-		'Accept-Encoding': 'gzip, deflate',
-		'X-GameWebToken': splatoon_access_token["result"]["accessToken"],
-		'Accept-Language': 'en-us',
-		'X-IsAnalyticsOptedIn': 'true',
-		'Connection': 'keep-alive',
-		'DNT': '0',
-		'User-Agent': 'Mozilla/5.0 (Linux; Android 7.1.2; Pixel Build/NJH47D; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/59.0.3071.125 Mobile Safari/537.36'
-	}
+	try:
+		app_head = {
+			'Host': 'app.splatoon2.nintendo.net',
+			'X-IsAppAnalyticsOptedIn': 'true',
+			'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+			'Accept-Encoding': 'gzip, deflate',
+			'X-GameWebToken': splatoon_access_token["result"]["accessToken"],
+			'Accept-Language': 'en-us',
+			'X-IsAnalyticsOptedIn': 'true',
+			'Connection': 'keep-alive',
+			'DNT': '0',
+			'User-Agent': 'Mozilla/5.0 (Linux; Android 7.1.2; Pixel Build/NJH47D; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/59.0.3071.125 Mobile Safari/537.36'
+		}
+	except:
+		print "Error from Nintendo:\n" + json.dumps(splatoon_access_token, indent=2)
+		exit(1)
 
 	url = "https://app.splatoon2.nintendo.net/?lang=en-US"
 
