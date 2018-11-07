8c4c61ed2a5a77cf0af2e24752edca490c25a307
eli fessler <eliwf8@gmail.com>
2018-11-07 05:03:22 -0800

    bump ver # for new SR stage update

--- splatnet2statink.py
+++ splatnet2statink.py
@@ -20,7 +20,7 @@ from distutils.version import StrictVersion
 from subprocess import call
 # PIL/Pillow imported at bottom
 
-A_VERSION = "1.3.1"
+A_VERSION = "1.3.2"
 
 print("splatnet2statink v{}".format(A_VERSION))
 
