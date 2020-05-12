bfeb07f4a6f00d21de7b417a07b3f21136e07b74
Ondřej Fiala <temp.xanomes@volny.cz>
2020-05-12 03:23:58 +0200

    add explicit GNU GPLv3

--- splatnet2statink.py
+++ splatnet2statink.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-# eli fessler
-# clovervidia
+#
+#   splatnet2statink.py
+#   Copyright (C) 2017-2020 eli fessler, clovervidia
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software Foundation,
+#   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+#
 from __future__ import unicode_literals
 from __future__ import print_function
 from __future__ import division
