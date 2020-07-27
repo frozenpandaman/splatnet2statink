7cfea65466dcdf3dac9d24cb5bc8f611b0c70249
tkgstrator <nasawake.am@gmail.com>
2020-07-27 02:31:30 -0400

    update iksm.py (#104)

--- splatnet2statink.py
+++ splatnet2statink.py
@@ -21,7 +21,7 @@ from distutils.version import StrictVersion
 from subprocess import call
 # PIL/Pillow imported at bottom
 
-A_VERSION = "1.5.4"
+A_VERSION = "1.5.5"
 
 print("splatnet2statink v{}".format(A_VERSION))
 
