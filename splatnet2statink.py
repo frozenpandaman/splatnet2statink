a5c2ba15db9229186d63173141245672cb7a8587
eli fessler <eliwf8@gmail.com>
2020-12-07 18:12:52 -0800

    update to v1.5.7

--- splatnet2statink.py
+++ splatnet2statink.py
@@ -21,7 +21,7 @@ from distutils.version import StrictVersion
 from subprocess import call
 # PIL/Pillow imported at bottom
 
-A_VERSION = "1.5.6"
+A_VERSION = "1.5.7"
 
 print("splatnet2statink v{}".format(A_VERSION))
 
