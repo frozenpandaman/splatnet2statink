ab13cee392ddc22470e0c226b01d928462c53da9
niyari <niyari@users.noreply.github.com>
2021-12-10 09:34:27 +0900

    Bump nsoapp_version (1.14.0)

--- iksm.py
+++ iksm.py
@@ -9,7 +9,7 @@ from bs4 import BeautifulSoup
 
 session = requests.Session()
 version = "unknown"
-nsoapp_version = "1.13.2"
+nsoapp_version = "1.14.0"
 
 # structure:
 # log_in() -> get_session_token()
