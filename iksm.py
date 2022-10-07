b7194891cc8d14bee8cea63b5dffdf3b88c570cb
eli fessler <eliwf8@gmail.com>
2022-10-07 13:43:36 -1000

    update NSO app version

--- iksm.py
+++ iksm.py
@@ -9,7 +9,7 @@ from bs4 import BeautifulSoup
 
 session = requests.Session()
 version = "unknown"
-nsoapp_version = "2.3.0"
+nsoapp_version = "2.3.1"
 
 # structure:
 # log_in() -> get_session_token()
